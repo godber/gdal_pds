9e68c8d6ae7ca593a7dffcc56cb58c2a888ca952
Austin Godber <godber@uberhip.com>
2014-01-05 18:50:23 -0700

    updated path to test data to match that used by the test data retrieval script

--- tests/gdal_pds_test.py
+++ tests/gdal_pds_test.py
@@ -5,7 +5,7 @@ import gdal_pds
 class TestGdalPds(unittest.TestCase):
 
     def setUp(self):
-        self.image_path = "data/1F345867992EFFB0J3P1212L0M1.img"
+        self.image_path = "test_data/1F345867992EFFB0J3P1212L0M1.img"
         self.image = gdal_pds.PDSImage(self.image_path)
 
     def test_image_path(self):
--- tests/label_test.py
+++ tests/label_test.py
@@ -5,7 +5,7 @@ from gdal_pds import label
 class TestLabel(unittest.TestCase):
 
     def setUp(self):
-        self.image_path = "data/1F345867992EFFB0J3P1212L0M1.img"
+        self.image_path = "test_data/1F345867992EFFB0J3P1212L0M1.img"
         self.label = label.read(self.image_path)
 
     def test_instrument_id(self):
