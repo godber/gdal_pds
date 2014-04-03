ff4013981f4ad58875c7555213820830f201e8aa
Austin Godber <godber@uberhip.com>
2014-04-02 17:20:51 -0700

    Improved label handling: wrapped and multiline included
    These changes fix the following issues:
    
    * wrapped labels
    * multiline labels denoted by ", () and {}
    * comments are now stored
    * empty lines are now stored
    
    Only the wrapped labels are tested at this point.   I expect bugs in the other
    types.
    
    refs #2, refs #3

--- gdal_pds/label.py
+++ gdal_pds/label.py
@@ -1,39 +1,131 @@
 """
-Known Issues::
+Implements the PDS Object Description Language as outlined here:
 
-    [ ] Doesn't handle multiline values
+    http://pds.jpl.nasa.gov/documents/sr/Chapter12.pdf
+
+TODO: This is still a work in progress.
 """
 
 from collections import OrderedDict
+
+
+class ParseError(Exception):
+    pass
+
+
+def set_value(label, key, value, in_block):
+    value = str(value).strip("\'\"")
+    if in_block:
+        label[in_block][key] = value
+    else:
+        label[key] = value
 
 
 def read(file_path):
     """
     Parses a PDS Label from a file, returns an ordered dict
     """
+
+    # Characters that can open a multiline value mapped to the closing
+    # characters
+    MULTILINE_DICT = {'"': '"', '(': ')', '{': '}'}
+
     label = OrderedDict()
     in_block = None
+    wrapped = False  # False or key
+    multiline = False
+    multiline_end = None
+    #multiline_prefixes = re.compile('^["(]')
     with open(file_path) as f:
-        for line in f.readlines():
+        for lineno, line in enumerate(f):
             # strip off the ^M and any trailing garbage
             line = line.rstrip()
+
+            # the label ends with END, stop here or you're parsing binary
+            # or other embedded headers
+            if line.strip() == 'END':
+                break
+
             if '=' in line:
+                # Lines with = in them are essentially key value pairs
+                # anything else is a wrapped line, comment, empty line or
+                # continued value
                 key, value = line.split('=')
                 key = key.strip()
                 value = value.strip()
+
+                # Since there is an = there should be a value, if there is not
+                # assume the line is wrapped.  These differ from  multiline
+                # values because the value is on a single line following the
+                # line which contains the key.
+                #
+                # Example:
+                #  TELEMETRY_SOURCE_NAME            =
+                #                               "025_001_p1212-009-0001_003_0345867992-031.dat"
+                if value == '':
+                    wrapped = key
+                elif value[0] in MULTILINE_DICT.keys():
+                    # Values beginning with " or ( can be multiline values, we
+                    # save the key and which prefix is used
+                    multiline = key
+                    # make sure there's no whitespace on the first char
+                    multiline_end = MULTILINE_DICT[value[0]]
+                    if value.endswith(multiline_end):
+                        # all on this line
+                        multiline = False
+                    else:
+                        pass
+                        #print key, multiline_end, value
+
+                # Handle OBJECT and GROUP blocks, as well as finally setting
+                # the key/value in the label
                 if key in ['OBJECT', 'GROUP']:
                     in_block = value
                     label[value] = OrderedDict([('_type', key)])
                 elif key in ['END_OBJECT', 'END_GROUP']:
                     in_block = None
                 else:
-                    value = value.strip("\'\"")
-                    if in_block:
-                        label[in_block][key] = value
-                    else:
-                        label[key] = value
+                    set_value(label, key, value, in_block)
+            else:
+                # No = in line
+                if wrapped:
+                    key = wrapped
+                    # strip off leading spaces (and trailing)
+                    value = line.strip()
+                    wrapped = False
+                elif multiline:
+                    key = multiline
 
-            if line.strip() == 'END':
-                break
+                    # clean up line, remove any whitespace on the right
+                    line = line.rstrip()
+                    # strip the leading 37 spaces, conditional may be
+                    # uneccessary
+                    if line.startswith(' ' * 37):
+                        line = line[37:]
+
+                    if in_block:
+                        value = label[in_block][key] + line
+                    else:
+                        value = label[key] + line
+
+                    if value.endswith(multiline_end):
+                        multiline = False
+                elif line == '':
+                    # empty lines are preserved with a special key value
+                    # '_emptyline'
+                    key = '_emptyline'
+                    value = True
+                elif line.startswith('/*'):
+                    # comments are preserved with a special key value
+                    # '_comment'
+                    key = '_comment'
+                    value = line
+                else:
+                    raise ParseError(
+                        'Unhandled condition in label on line %s:\n%s' %
+                        (str(lineno + 1), line)
+                    )
+
+                set_value(label, key, value, in_block)
 
     return label
--- tests/label_test.py
+++ tests/label_test.py
@@ -7,7 +7,24 @@ class TestLabel(unittest.TestCase):
     def setUp(self):
         self.image_path = "test_data/1F345867992EFFB0J3P1212L0M1.img"
         self.label = label.read(self.image_path)
+        #print self.label
 
     def test_instrument_id(self):
         assert self.label['COMMAND_SEQUENCE_NUMBER'] == '1'
         assert self.label['INSTRUMENT_ID'] == "FRONT_HAZCAM_LEFT"
+
+    def test_wrapped_label(self):
+        assert self.label['TELEMETRY_SOURCE_NAME'] == \
+            '025_001_p1212-009-0001_003_0345867992-031.dat'
+
+    def test_multiline_string_1(self):
+        """
+        This one is tricky because the continued string begins with a space
+        on the second line, just using strip would consume this.
+        """
+        assert self.label['DATA_SET_NAME'] == \
+            "MER 1 MARS HAZARD AVOIDANCE CAMERA EDR OPS VERSION 1.0"
+
+    def test_multiline_string_2(self):
+        assert self.label['PRODUCER_INSTITUTION_NAME'] == \
+            'MULTIMISSION IMAGE PROCESSING SUBSYSTEM, JET PROPULSION LAB'
